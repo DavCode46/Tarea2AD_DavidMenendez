de2e4857d386d702632a00e859a7c3c2c2ead4b0
David <davidmenendezblanco@alumnos.tuniversformacion.es>
2024-11-19 22:37:35 +0100

    Feat: obtenerTodasParadas method done

--- src/main/java/davidmb/dao/ParadaDAO.java
+++ src/main/java/davidmb/dao/ParadaDAO.java
@@ -12,32 +12,31 @@ import java.util.logging.Logger;
 import davidmb.models.Parada;
 import davidmb.models.Peregrino;
 
-public class ParadaDAO  {
-	
+public class ParadaDAO {
+
 	ConexionDB con = ConexionDB.getInstancia();
 	Logger logger = Logger.getLogger(ParadaDAO.class.getName());
-	
+
 	public Optional<Long> insertar(Parada parada) {
 		String sqlParadas = "INSERT INTO Paradas (nombre, region, responsable, id_usuario) VALUES (?, ?, ?, ?)";
 		String sqlUsuarios = "SELECT MAX(id) FROM Usuarios";
-		
-		try(Connection connection = con.getConexion();
-			PreparedStatement paradaStmt = connection.prepareStatement(sqlParadas);
-			PreparedStatement usuarioStmt = connection.prepareStatement(sqlUsuarios);
-			) {
-			
+
+		try (Connection connection = con.getConexion();
+				PreparedStatement paradaStmt = connection.prepareStatement(sqlParadas);
+				PreparedStatement usuarioStmt = connection.prepareStatement(sqlUsuarios);) {
+
 			Long nextUsuarioId = 1L;
 			try (ResultSet rs = usuarioStmt.executeQuery()) {
-				if(rs.next()) {
+				if (rs.next()) {
 					nextUsuarioId = rs.getLong(1);
 				}
 			}
-			
+
 			paradaStmt.setString(1, parada.getNombre());
 			paradaStmt.setString(2, String.valueOf(parada.getRegion()));
 			paradaStmt.setString(3, parada.getResponsable());
 			paradaStmt.setLong(4, nextUsuarioId);
-			
+
 			int rowsAffected = paradaStmt.executeUpdate();
 			System.out.println("Parada insertada, filas afectadas: " + rowsAffected);
 			return Optional.of(nextUsuarioId);
@@ -47,81 +46,118 @@ public class ParadaDAO  {
 		return Optional.empty();
 	}
 
-
 	public Parada obtenerParadaPorIdUsuario(Long id) {
 		String paradaSql = "SELECT * FROM Paradas WHERE id_usuario = ?";
-		
+
 		Parada parada = null;
 		Long paradaId = null;
-		
-		try(Connection connection = con.getConexion();
-			PreparedStatement paradaStmt = connection.prepareStatement(paradaSql);
-            ) {
-			    paradaStmt.setLong(1, id);
-				try (ResultSet rs = paradaStmt.executeQuery()) {
-					if (rs.next()) {
-						parada = new Parada();
-						parada.setId(rs.getLong("id"));
-						parada.setNombre(rs.getString("nombre"));
-						parada.setRegion(rs.getString("region").charAt(0));
-						parada.setResponsable(rs.getString("responsable"));
-					}
+
+		try (Connection connection = con.getConexion();
+				PreparedStatement paradaStmt = connection.prepareStatement(paradaSql);) {
+			paradaStmt.setLong(1, id);
+			try (ResultSet rs = paradaStmt.executeQuery()) {
+				if (rs.next()) {
+					parada = new Parada();
+					parada.setId(rs.getLong("id"));
+					parada.setNombre(rs.getString("nombre"));
+					parada.setRegion(rs.getString("region").charAt(0));
+					parada.setResponsable(rs.getString("responsable"));
 				}
-			                       
-			} catch (SQLException e) {
-				logger.severe("Error al obtener parada por id de usuario: " + e.getMessage());
 			}
+
+		} catch (SQLException e) {
+			logger.severe("Error al obtener parada por id de usuario: " + e.getMessage());
+		}
 		return parada;
 	}
-	
+
 	public List<Peregrino> obtenerPeregrinosParada(Long idParada) {
+		String sqlPeregrinosParadas = "SELECT id_peregrino FROM Peregrinos_paradas WHERE id_parada = ?";
+		String sqlPeregrino = "SELECT * FROM Peregrinos WHERE id = ?";
+		List<Peregrino> peregrinos = new ArrayList<>();
+
+		try (Connection connection = con.getConexion();
+				PreparedStatement paradasStmt = connection.prepareStatement(sqlPeregrinosParadas)) {
+
+			paradasStmt.setLong(1, idParada);
+			try (ResultSet rsParadas = paradasStmt.executeQuery()) {
+				while (rsParadas.next()) {
+					Long idPeregrino = rsParadas.getLong("id_peregrino");
+
+					try (PreparedStatement peregrinoStmt = connection.prepareStatement(sqlPeregrino)) {
+						peregrinoStmt.setLong(1, idPeregrino);
+						try (ResultSet rsPeregrino = peregrinoStmt.executeQuery()) {
+							if (rsPeregrino.next()) {
+								Peregrino peregrino = new Peregrino();
+								peregrino.setId(rsPeregrino.getLong("id"));
+								peregrino.setNombre(rsPeregrino.getString("nombre"));
+								peregrino.setNacionalidad(rsPeregrino.getString("nacionalidad"));
+
+								peregrinos.add(peregrino);
+							}
+						}
+					}
+				}
+			}
+		} catch (SQLException e) {
+			logger.severe("Error al obtener peregrinos de la parada: " + e.getMessage());
+		}
+
+		return peregrinos;
+	}
+
+	public List<Parada> obtenerTodasParadas() {
+	    String sql = "SELECT * FROM Paradas";
 	    String sqlPeregrinosParadas = "SELECT id_peregrino FROM Peregrinos_paradas WHERE id_parada = ?";
-	    String sqlPeregrino = "SELECT * FROM Peregrinos WHERE id = ?";
-	    List<Peregrino> peregrinos = new ArrayList<>();
-	    
+	    List<Parada> paradas = new ArrayList<>();
+
 	    try (Connection connection = con.getConexion();
-	         PreparedStatement paradasStmt = connection.prepareStatement(sqlPeregrinosParadas)) {
-	        
-	        paradasStmt.setLong(1, idParada);
-	        try (ResultSet rsParadas = paradasStmt.executeQuery()) {
-	            while (rsParadas.next()) {
-	                Long idPeregrino = rsParadas.getLong("id_peregrino");
-	                        
-	                try (PreparedStatement peregrinoStmt = connection.prepareStatement(sqlPeregrino)) {
-	                    peregrinoStmt.setLong(1, idPeregrino);
-	                    try (ResultSet rsPeregrino = peregrinoStmt.executeQuery()) {
-	                        if (rsPeregrino.next()) {
-	                            Peregrino peregrino = new Peregrino();
-	                            peregrino.setId(rsPeregrino.getLong("id"));
-	                            peregrino.setNombre(rsPeregrino.getString("nombre"));
-	                            peregrino.setNacionalidad(rsPeregrino.getString("nacionalidad"));
-	                            
-	                            peregrinos.add(peregrino);
-	                        }
+	         PreparedStatement stmt = connection.prepareStatement(sql);
+	         ResultSet rs = stmt.executeQuery()) {
+
+	        while (rs.next()) {
+	            Parada parada = new Parada();
+	            parada.setId(rs.getLong("id"));
+	            parada.setNombre(rs.getString("nombre"));
+	            parada.setRegion(rs.getString("region").charAt(0));
+	            parada.setResponsable(rs.getString("responsable"));
+	            parada.setIdUsuario(rs.getLong("id_usuario"));
+            
+	            List<Long> idsPeregrinos = new ArrayList<>();
+	         
+	            try (PreparedStatement paradasStmt = connection.prepareStatement(sqlPeregrinosParadas)) {
+	                paradasStmt.setLong(1, parada.getId());
+	                try (ResultSet rsParadas = paradasStmt.executeQuery()) {
+	                    while (rsParadas.next()) {
+	                        Long idPeregrino = rsParadas.getLong("id_peregrino");
+	                        idsPeregrinos.add(idPeregrino);
 	                    }
 	                }
 	            }
+    
+	            parada.setPeregrinos(idsPeregrinos);
+         
+	            paradas.add(parada);
 	        }
+
 	    } catch (SQLException e) {
-	        logger.severe("Error al obtener peregrinos de la parada: " + e.getMessage());
+	        logger.severe("Error al obtener todas las paradas: " + e.getMessage());
 	    }
-	    
-	    return peregrinos;
+	    return paradas;
 	}
 
 
-	public List<Parada> obtenerTodos() {
-		System.out.println("Obteniendo todas las paradas");
-		return new ArrayList<Parada>();
-	}
-	
 	public static void main(String args[]) {
 		ParadaDAO dao = new ParadaDAO();
-        
-       List<Peregrino> peregrinos = dao.obtenerPeregrinosParada(1L);
+
+		List<Peregrino> peregrinos = dao.obtenerPeregrinosParada(1L);
 		for (Peregrino p : peregrinos) {
 			System.out.println(p);
 		}
+//		List<Parada> paradas = dao.obtenerTodasParadas();
+//		for (Parada p : paradas) {
+//			System.out.println(p);
+//		}
 	}
 
 }
--- src/main/java/davidmb/dao/UsuarioDAO.java
+++ src/main/java/davidmb/dao/UsuarioDAO.java
@@ -12,66 +12,68 @@ import davidmb.models.Usuario;
 public class UsuarioDAO {
 	private static final Logger logger = Logger.getLogger(UsuarioDAO.class.getName());
 	ConexionDB con = ConexionDB.getInstancia();
-	
+
 	public Long insertar(Usuario u) {
-	    String sqlUsuario = "INSERT INTO Usuarios (usuario, password, perfil) VALUES (?, ?, ?)";
-	    Long idUsuario = null;
+		String sqlUsuario = "INSERT INTO Usuarios (usuario, password, perfil) VALUES (?, ?, ?)";
+		Long idUsuario = null;
 
-	    try (Connection connection = con.getConexion();
-	         PreparedStatement usuarioStmt = connection.prepareStatement(sqlUsuario, Statement.RETURN_GENERATED_KEYS)) {
-	        
-	        usuarioStmt.setString(1, u.getNombreUsuario());
-	        usuarioStmt.setString(2, u.getPassword());
-	        usuarioStmt.setString(3, u.getPerfil());
-	        int rowsAffected = usuarioStmt.executeUpdate();
+		try (Connection connection = con.getConexion();
+				PreparedStatement usuarioStmt = connection.prepareStatement(sqlUsuario,
+						Statement.RETURN_GENERATED_KEYS)) {
 
-	        if (rowsAffected > 0) {
-	            try (ResultSet generatedKeys = usuarioStmt.getGeneratedKeys()) {
-	                if (generatedKeys.next()) {
-	                    idUsuario = generatedKeys.getLong(1); // Obtener el ID generado
-	                }
-	            }
-	        }
-	        System.out.println("Usuario insertado con ID: " + idUsuario);
-	    } catch (SQLException ex) {
-	        logger.severe("Error al insertar usuario: " + ex.getMessage());
-	    }
-	    return idUsuario; // Retorna el ID generado
+			usuarioStmt.setString(1, u.getNombreUsuario());
+			usuarioStmt.setString(2, u.getPassword());
+			usuarioStmt.setString(3, u.getPerfil());
+			int rowsAffected = usuarioStmt.executeUpdate();
+
+			if (rowsAffected > 0) {
+				try (ResultSet generatedKeys = usuarioStmt.getGeneratedKeys()) {
+					if (generatedKeys.next()) {
+						idUsuario = generatedKeys.getLong(1); // Obtener el ID generado
+					}
+				}
+			}
+			System.out.println("Usuario insertado con ID: " + idUsuario);
+		} catch (SQLException ex) {
+			logger.severe("Error al insertar usuario: " + ex.getMessage());
+		}
+		return idUsuario; // Retorna el ID generado
 	}
-	
+
 	public Usuario login(String usuario, String password) {
 		Usuario u = null;
 		String sql = "SELECT * FROM Usuarios WHERE usuario = ? AND password = ?";
-		
-		try(Connection connection = con.getConexion();
-			PreparedStatement stmt = connection.prepareStatement(sql);) {
-			
+
+		try (Connection connection = con.getConexion();
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+			) {
+
 			stmt.setString(1, usuario);
 			stmt.setString(2, password);
-			ResultSet rs = stmt.executeQuery();
-			
-		    if(rs.next()) {
-		    	u = new Usuario();
-		    	u.setId(rs.getLong("id"));
-		    	u.setNombreUsuario(rs.getString("usuario"));
-		    	//u.setPassword(rs.getString("password"));
-		    	u.setPerfil(rs.getString("perfil"));
-		    	
-		    } 
-			
-		} catch(SQLException ex) {
+
+			if (rs.next()) {
+				u = new Usuario();
+				u.setId(rs.getLong("id"));
+				u.setNombreUsuario(rs.getString("usuario"));
+				// u.setPassword(rs.getString("password"));
+				u.setPerfil(rs.getString("perfil"));
+
+			}
+
+		} catch (SQLException ex) {
 			logger.severe("Error al buscar usuario: " + ex.getMessage());
 		}
 		return u;
 	}
-	
+
 	public static void main(String args[]) {
 		UsuarioDAO uDAO = new UsuarioDAO();
 //		Usuario u = new Usuario("David", "david", "peregrino");
 //		
 //		uDAO.insertar(u);
 		Usuario u = uDAO.login("responsable1", "respass1");
-		if(u != null) {
+		if (u != null) {
 			System.out.println("Usuario encontrado: " + u);
 		} else {
 			System.out.println("Usuario no encontrado");
